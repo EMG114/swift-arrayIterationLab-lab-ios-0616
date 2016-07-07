629daa3adc750a377ff09fc91b2bb018336999b1
EMG114 <egutierrez411@gmail.com>
2016-07-07 13:26:03 -0400

    arrays

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -18,14 +18,25 @@
 
 
 
-
+let cart :[String] = ["Chips", "Salsa", "Guacamole", "Red wine"]
 
 /*: question2
  ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code? Explicitly mark the content of the cart as numbers.
  */
 // write your code here
 
+//var num : [Int] = []
+//
+//for i in 1...10 {
+//    num.append(i)
+//
+//
+//}
 
+
+
+
+var numbersTo10: [Int] = [1,2,3,4,5,6,7,8,9,10]
 
 
 
@@ -41,15 +52,19 @@
 
 
 
+for number in numbersTo10 {
+       print (number)
+    }
 
 /*: question4
  ### 4. Take the list of shopping cart items you created in question 1 and print their values in the most efficient way possible. Prefix each item in the array with a string of text.
  */
 // write your code here
+for item in cart {
 
 
-
-
+        print("I need to buy a: \(item)")
+    }
 
 
 
@@ -59,10 +74,11 @@
 // write your code here
 
 
+func printItemValue(item: String) {
+       print("Need one: \(item)")
 
 
-
-
+}
 
 /*: question6
  ### 4. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
@@ -70,7 +86,16 @@
 // write your code here
 
 
+let people = ["Maryann", "Francis", "Joe", "Michael"]
 
+func greetPeople(people: [String]) {
+        for person in people {
+                print("Good morning \(person)")
+               if person == "Michael" {
+                        print("Top of the morning \(person)!")
+                    }
+            }
+    }
 
 
 
@@ -81,7 +106,23 @@
 
 
 
+func numbersLessThan50(grades: [Int]) -> [Int] {
+        var numbersLessThan50: [Int] = []
+    
+        for number in numbers {
+               if number <= 50 {
+                       numbersLessThan50.append(number)
+                    }
+           }
+    
+        return numbersLessThan50
+    }
 
+let numbers = [40, 60, 50, 52, 59, 13, 90, 100, 5, 52, 51, 49]
+
+var finalNumbers = numbersLessThan50(numbers)
+
+    print (finalNumbers)
 
 
 
